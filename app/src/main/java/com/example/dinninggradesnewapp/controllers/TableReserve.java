0ace8fc931aa0e277d0144e2fbb36520765c7692
Kavindu Chethani <46352484+kavindu-chethani@users.noreply.github.com>
2022-05-07 17:24:28 +0530

    Adduserdetailsfor booking

--- app/src/main/java/com/example/dinninggradesnewapp/controllers/TableReserve.java
+++ app/src/main/java/com/example/dinninggradesnewapp/controllers/TableReserve.java
@@ -1,24 +1,29 @@
-package com.example.dinninggradesnewapp;
+package com.example.dinninggradesnewapp.controllers;
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+import com.example.dinninggradesnewapp.R;
+
+public class TableReserve extends AppCompatActivity {
     public Button move;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_table_reserve);
 
-        move = findViewById(R.id.reserve);
+        move = findViewById(R.id.submit);
         move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, TableView.class);
+                Intent intent = new Intent(TableReserve.this, ReservedTable.class);
                 startActivity(intent);
             }
         });
     }
+
 }
---
+++ app/src/main/java/com/example/dinninggradesnewapp/models/tableUserModel.java
@@ -0,0 +1,51 @@
+package com.example.dinninggradesnewapp.models;
+
+
+public class tableUserModel {
+        private String name;
+        private String contactno;
+        private String nic;
+        private String email;
+
+        public tableUserModel(){}
+
+        public tableUserModel(String name, String contactno, String nic, String email) {
+            this.name = name;
+            this.contactno = contactno;
+            this.nic = nic;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getContactno() {
+            return contactno;
+        }
+
+        public void setContactno(String contactno) {
+            this.contactno = contactno;
+        }
+
+        public String getNic() {
+            return nic;
+        }
+
+        public void setNic(String nic) {
+            this.nic = nic;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
