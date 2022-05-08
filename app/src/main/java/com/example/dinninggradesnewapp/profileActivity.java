72e7a1bbc9c986800b6cae312340804a9af699e9
Paramee Masakorala <parameemasakorala98@gmail.com>
2022-05-08 07:22:09 +0530

    update crud operations

---
+++ app/src/main/java/com/example/dinninggradesnewapp/profileActivity.java
@@ -0,0 +1,87 @@
+package com.example.dinninggradesnewapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class profileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
+
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout = (Button) findViewById(R.id.cirLogoutButton);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(profileActivity.this, loginActivity.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView fullNameTextView = (TextView) findViewById(R.id.FullName);
+        final TextView emailTextView = (TextView) findViewById(R.id.Email);
+        final TextView contactTextView = (TextView) findViewById(R.id.Contact);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                com.example.projectmad.user userProfile = snapshot.getValue(com.example.projectmad.user.class);
+
+                if (userProfile != null){
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+                    String contact = userProfile.contact;
+
+                    fullNameTextView.setText(fullName);
+                    emailTextView.setText(email);
+                    contactTextView.setText(contact);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(profileActivity.this, "Something wrong happend!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+
+
+
+
+    public void onPNextClick(View view)
+    {
+        startActivity(new Intent(profileActivity.this,paymentActivity.class));
+
+    }
+}
+
---
+++ app/src/main/java/com/example/dinninggradesnewapp/user.java
@@ -0,0 +1,19 @@
+package com.example.dinninggradesnewapp;
+
+public class user {
+
+    public String fullName, email, contact, password;
+
+    public user(){
+
+    }
+
+    public user(String fullName, String email, String contact, String password){
+        this.fullName = fullName;
+        this.email = email;
+        this.contact = contact;
+        this.password = password;
+    }
+
+}
+
